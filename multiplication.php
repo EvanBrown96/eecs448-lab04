a268e19ef63d3f2ff7abb375569b085ba6c71a2f
EvanBrown96 <evan.br96@yahoo.com>
2018-09-28 20:17:14 -0500

    add start of multiplication file - header row

---
+++ multiplication.php
@@ -0,0 +1,15 @@
+<?php
+
+  echo("<table style='border: 1px solid black;'><tr style='background-color: red; color: white;'><td></td>");
+
+  $mult1 = 1;
+
+  for($i = 1; $i < 101; $i++){
+    echo("<td>" . $i . "</td>");
+  }
+
+  echo("</tr>");
+
+  echo("</table>")
+
+?>
