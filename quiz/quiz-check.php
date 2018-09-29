ab777a14f8e63b2eb2e9476063e3047a2f483b11
EvanBrown96 <evan.br96@yahoo.com>
2018-09-29 00:04:38 -0500

    ajax call now passes in header if it wants to get questions

--- quiz/quiz-check.php
+++ quiz/quiz-check.php
@@ -1,19 +1,28 @@
 <?php
 
   // array of question object in the format {question: "question text",
-  //                                         answers: [answer1, answer2...],
-  //                                         correct: index of correct answer}
+  //                                         answers: [answer1, answer2...]}
   $questions = array(
     array('question' => "What is 2 + 2?",
-          'answers' => array(1, 10, 4, -70),
-          'correct' => 2),
+          'answers' => array(1, 10, 4, -70)),
 
     array('question' => "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
-          'answers' => array("Not Enough", "Just Right", "Too much", "700 pounds"),
-          'correct' => 3)
+          'answers' => array("Not Enough", "Just Right", "Too much", "700 pounds"))
   );
 
-  echo json_encode($questions);
+  // array of correct answer indexes
+  $answers = array(2, 3);
+
+  if($_SERVER['HTTP_QUIZ_MODE'] == "GET_ANSWERS"){
+    // frontend is requesting answers
+    echo json_encode($questions);
+
+  }
+  else{
+    // display score
+    echo "poop";
+
+  }
 
   // foreach($_POST as $val){
   //   echo $val;
