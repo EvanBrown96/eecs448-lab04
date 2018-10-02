2134cb6289c649502dfc1403dd42e2b1e4c47ed8
EvanBrown96 <evan.br96@yahoo.com>
2018-10-01 23:56:29 -0500

    fixed semicolons and table formatting

--- shop/customerBackend.php
+++ shop/customerBackend.php
@@ -41,16 +41,18 @@
   }
   else{
 
-    echo "<h5>Thank you for your purchase.</h5>";
+    echo "<h3>Thank you for your purchase!</h3>";
     echo "<p>Username: " . $_POST['username'] . "</p>";
     echo "<p>Password: " . $_POST['password'] . "</p>";
 
     echo "<table><tr><td></td><td>Quantity</td><td>Cost Per Item</td><td>Subtotal</td></tr>";
     foreach($_POST as $key => $value){
-      $index = (int)substr($key, 5, 1)
-      echo "<tr><td>" . $value . "</td><td>" . $items[$index]["name"] . "</td><td>$" . $items[$index]["price"] . "</td><td>$" . $value * $items[$index]["price"] . "</td></tr>";
+      if(substr($key, 0, 4) == "item" && $value > 0){
+        $index = (int)substr($key, 5, 1);
+        echo "<tr><td>" . $items[$index]["name"] . "</td><td>" . $value . "</td><td>$" . $items[$index]["price"] . "</td><td>$" . $value * $items[$index]["price"] . "</td></tr>";
+      }
     }
-    echo("</table>")
+    echo("</table>");
   }
 
  ?>
