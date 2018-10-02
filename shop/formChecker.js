118bc8ae61ee46d49241f4234fab47a400ced2e0
EvanBrown96 <evan.br96@yahoo.com>
2018-10-01 21:09:00 -0500

    added error modal and start of formChecker.js

--- shop/formChecker.js
+++ shop/formChecker.js
@@ -1,6 +1,33 @@
 function promptAuthentication(){
+
+  let fail = false;
+  let msg;
+
   for(let i = 0; i < items.length; i++){
     let item_elem = document.getElementById("item_"+i);
-    console.log(item_elem.value);
+    let item_val = item_elem.value;
+
+    if(item_val == ""){
+      fail = true;
+      msg = "Quantities cannot be blank!";
+      break;
+    }
+    if(Number(item_val) < 0){
+      fail = true;
+      msg = "Quantities cannot be negative!";
+      break;
+    }
   }
+
+  if(fail){
+    showError(msg);
+  }
+  else{
+    $('#authentication').modal('show');
+  }
+}
+
+function showError(msg){
+  document.getElementById('error_msg').innerHTML = msg;
+  $('#error-modal').modal('show');
 }
